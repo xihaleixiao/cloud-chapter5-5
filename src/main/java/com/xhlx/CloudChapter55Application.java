ab6772907c8f2860dd5f4c93dd6c5941b6e730d8
zhanglei_yq <zhanglei_yq@kedacom.com>
2019-11-01 10:11:40 +0800

    Initial commit

---
+++ src/main/java/com/xhlx/CloudChapter55Application.java
@@ -0,0 +1,13 @@
+package com.xhlx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudChapter55Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudChapter55Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/xhlx/CloudChapter55ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhlx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudChapter55ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
